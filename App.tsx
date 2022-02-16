275311afaf7331777c479c3a65c8d7e213bb25c7
Jossic <jossic.lapierre@gmail.com>
2022-02-16 22:29:46 +0100

    Navigation work

--- App.tsx
+++ App.tsx
@@ -1,15 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import Navigation from './navigation';
-import HomeScreen from './screens/HomeScreen';
 
 export default function App() {
 	return (
-		<View style={styles.container}>
-			{/* <HomeScreen /> */}
+		<>
 			<Navigation />
 			<StatusBar style='auto' />
-		</View>
+		</>
 	);
 }
 
--- navigation/index.tsx
+++ navigation/index.tsx
@@ -1,5 +1,5 @@
-import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
 import HomeScreen from '../screens/HomeScreen';
 import PlannerScreen from '../screens/PlannerScreen';
 
