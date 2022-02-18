d101ee088b991495b668117b17cb8b0d70609e28
Jossic <jossic.lapierre@gmail.com>
2022-02-18 17:30:47 +0100

    Adding modal

--- screens/WorkoutDetailScreen.tsx
+++ screens/WorkoutDetailScreen.tsx
@@ -1,8 +1,9 @@
 import { NativeStackHeaderProps } from '@react-navigation/native-stack';
-import React from 'react';
+import React, { useState } from 'react';
 import {
 	ActivityIndicator,
 	Alert,
+	Modal,
 	Pressable,
 	StyleSheet,
 	Text,
@@ -18,6 +19,7 @@ type Navigation = WorkoutDetailScreenProps & NativeStackHeaderProps;
 
 const WorkoutDetailScreen: React.FC<Navigation> = ({ route }) => {
 	const workout = useWorkoutBySlug(route.params.slug);
+	const [modalVisible, setModalVisible] = useState<boolean>(false);
 
 	if (!workout) {
 		return <ActivityIndicator />;
@@ -26,7 +28,29 @@ const WorkoutDetailScreen: React.FC<Navigation> = ({ route }) => {
 	return (
 		<View style={styles.container}>
 			<Text>WorkoutDetailScreen - {workout?.name}</Text>
-			<PressableText text='Cheeeeck' onPress={() => alert('Alert !')} />
+			<PressableText
+				text='Cheeeeck'
+				onPress={() => setModalVisible(true)}
+			/>
+			<Modal
+				animationType='slide'
+				transparent={true}
+				visible={modalVisible}
+				onRequestClose={() => {
+					Alert.alert('Modal has been closed.');
+					setModalVisible(!modalVisible);
+				}}>
+				<View style={styles.centeredView}>
+					<View style={styles.modalView}>
+						<Text style={styles.modalText}>Hello World!</Text>
+						<Pressable
+							style={[styles.button, styles.buttonClose]}
+							onPress={() => setModalVisible(!modalVisible)}>
+							<Text style={styles.textStyle}>Hide Modal</Text>
+						</Pressable>
+					</View>
+				</View>
+			</Modal>
 		</View>
 	);
 };
@@ -39,5 +63,46 @@ const styles = StyleSheet.create({
 		alignItems: 'center',
 		justifyContent: 'center',
 		paddingHorizontal: 20,
+	},
+	centeredView: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		marginTop: 22,
+	},
+	modalView: {
+		margin: 20,
+		backgroundColor: 'white',
+		borderRadius: 20,
+		padding: 35,
+		alignItems: 'center',
+		shadowColor: '#000',
+		shadowOffset: {
+			width: 0,
+			height: 2,
+		},
+		shadowOpacity: 0.25,
+		shadowRadius: 4,
+		elevation: 5,
+	},
+	button: {
+		borderRadius: 20,
+		padding: 10,
+		elevation: 2,
+	},
+	buttonOpen: {
+		backgroundColor: '#F194FF',
+	},
+	buttonClose: {
+		backgroundColor: '#2196F3',
+	},
+	textStyle: {
+		color: 'white',
+		fontWeight: 'bold',
+		textAlign: 'center',
+	},
+	modalText: {
+		marginBottom: 15,
+		textAlign: 'center',
 	},
 });
