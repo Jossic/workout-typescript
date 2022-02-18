3dff8ffdf447aa03d3bdeab923e7c6cbfdd50201
Jossic <jossic.lapierre@gmail.com>
2022-02-18 21:45:18 +0100

    Modal isolation + design

---
+++ components/styled/Modal.tsx
@@ -0,0 +1,96 @@
+import React, { useState } from 'react';
+import {
+	Pressable,
+	StyleSheet,
+	Text,
+	View,
+	Modal as RNModal,
+	Alert,
+	useWindowDimensions,
+} from 'react-native';
+import PressableText from './PressableText';
+
+interface ModalProps {}
+
+const Modal: React.FC<ModalProps> = () => {
+	const { height, width } = useWindowDimensions();
+	const [modalVisible, setModalVisible] = useState<boolean>(false);
+	return (
+		<View>
+			<RNModal
+				animationType='slide'
+				transparent={true}
+				visible={modalVisible}
+				onRequestClose={() => {
+					Alert.alert('Modal has been closed.');
+					setModalVisible(!modalVisible);
+				}}>
+				<View style={styles.centeredView}>
+					<View
+						style={[
+							styles.modalView,
+							{ height: height * 0.7, width: width * 0.8 },
+						]}>
+						<Text style={styles.modalText}>Hello World!</Text>
+
+						<Pressable
+							style={[styles.button, styles.buttonClose]}
+							onPress={() => setModalVisible(!modalVisible)}>
+							<Text style={styles.textStyle}>Hide Modal</Text>
+						</Pressable>
+					</View>
+				</View>
+			</RNModal>
+			<PressableText
+				text='Cheeck'
+				onPress={() => setModalVisible(!modalVisible)}
+			/>
+		</View>
+	);
+};
+
+export default Modal;
+
+const styles = StyleSheet.create({
+	centeredView: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		marginTop: 22,
+	},
+	modalView: {
+		margin: 20,
+		backgroundColor: 'white',
+		borderRadius: 20,
+		// padding: 100,
+		alignItems: 'center',
+		shadowColor: '#000',
+		shadowOffset: {
+			width: 0,
+			height: 2,
+		},
+		shadowOpacity: 0.25,
+		shadowRadius: 4,
+		elevation: 5,
+	},
+	button: {
+		borderRadius: 20,
+		padding: 10,
+		elevation: 2,
+	},
+	buttonOpen: {
+		backgroundColor: '#F194FF',
+	},
+	buttonClose: {
+		backgroundColor: '#2196F3',
+	},
+	textStyle: {
+		color: 'white',
+		fontWeight: 'bold',
+		textAlign: 'center',
+	},
+	modalText: {
+		marginBottom: 15,
+		textAlign: 'center',
+	},
+});
--- screens/WorkoutDetailScreen.tsx
+++ screens/WorkoutDetailScreen.tsx
@@ -1,15 +1,7 @@
 import { NativeStackHeaderProps } from '@react-navigation/native-stack';
 import React, { useState } from 'react';
-import {
-	ActivityIndicator,
-	Alert,
-	Modal,
-	Pressable,
-	StyleSheet,
-	Text,
-	View,
-} from 'react-native';
-import PressableText from '../components/styled/PressableText';
+import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
+import Modal from '../components/styled/Modal';
 import { useWorkoutBySlug } from '../hooks/useWorkoutBySlug';
 
 type WorkoutDetailScreenProps = {
@@ -19,7 +11,6 @@ type Navigation = WorkoutDetailScreenProps & NativeStackHeaderProps;
 
 const WorkoutDetailScreen: React.FC<Navigation> = ({ route }) => {
 	const workout = useWorkoutBySlug(route.params.slug);
-	const [modalVisible, setModalVisible] = useState<boolean>(false);
 
 	if (!workout) {
 		return <ActivityIndicator />;
@@ -28,29 +19,7 @@ const WorkoutDetailScreen: React.FC<Navigation> = ({ route }) => {
 	return (
 		<View style={styles.container}>
 			<Text>WorkoutDetailScreen - {workout?.name}</Text>
-			<PressableText
-				text='Cheeeeck'
-				onPress={() => setModalVisible(true)}
-			/>
-			<Modal
-				animationType='slide'
-				transparent={true}
-				visible={modalVisible}
-				onRequestClose={() => {
-					Alert.alert('Modal has been closed.');
-					setModalVisible(!modalVisible);
-				}}>
-				<View style={styles.centeredView}>
-					<View style={styles.modalView}>
-						<Text style={styles.modalText}>Hello World!</Text>
-						<Pressable
-							style={[styles.button, styles.buttonClose]}
-							onPress={() => setModalVisible(!modalVisible)}>
-							<Text style={styles.textStyle}>Hide Modal</Text>
-						</Pressable>
-					</View>
-				</View>
-			</Modal>
+			<Modal />
 		</View>
 	);
 };
@@ -63,46 +32,5 @@ const styles = StyleSheet.create({
 		alignItems: 'center',
 		justifyContent: 'center',
 		paddingHorizontal: 20,
-	},
-	centeredView: {
-		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center',
-		marginTop: 22,
-	},
-	modalView: {
-		margin: 20,
-		backgroundColor: 'white',
-		borderRadius: 20,
-		padding: 35,
-		alignItems: 'center',
-		shadowColor: '#000',
-		shadowOffset: {
-			width: 0,
-			height: 2,
-		},
-		shadowOpacity: 0.25,
-		shadowRadius: 4,
-		elevation: 5,
-	},
-	button: {
-		borderRadius: 20,
-		padding: 10,
-		elevation: 2,
-	},
-	buttonOpen: {
-		backgroundColor: '#F194FF',
-	},
-	buttonClose: {
-		backgroundColor: '#2196F3',
-	},
-	textStyle: {
-		color: 'white',
-		fontWeight: 'bold',
-		textAlign: 'center',
-	},
-	modalText: {
-		marginBottom: 15,
-		textAlign: 'center',
 	},
 });
