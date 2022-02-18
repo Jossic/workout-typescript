8fe07286e7c8c638cc68758e8dd5e3b667e2f2fe
Jossic <jossic.lapierre@gmail.com>
2022-02-18 15:23:57 +0100

    Adding type to slug

--- screens/WorkoutDetailScreen.tsx
+++ screens/WorkoutDetailScreen.tsx
@@ -2,9 +2,12 @@ import { NativeStackHeaderProps } from '@react-navigation/native-stack';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-type WorkoutDetailScreenProps = NativeStackHeaderProps;
+type WorkoutDetailScreenProps = {
+	route: { params: { slug: string } };
+};
+type Navigation = WorkoutDetailScreenProps & NativeStackHeaderProps;
 
-const WorkoutDetailScreen: React.FC<WorkoutDetailScreenProps> = ({ route }) => {
+const WorkoutDetailScreen: React.FC<Navigation> = ({ route }) => {
 	return (
 		<View style={styles.container}>
 			<Text>WorkoutDetailScreen - {route.params.slug}</Text>
