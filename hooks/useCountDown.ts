e5f38ab5af0202d4d5f3e6c1c4ccb8cc20b04125
Jossic <jossic.lapierre@gmail.com>
2022-02-19 14:28:23 +0100

    cleanup function

--- hooks/useCountDown.ts
+++ hooks/useCountDown.ts
@@ -1,21 +1,35 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 export const useCountDown = (idx: number, initialCount: number) => {
+	const intervalRef = useRef<number>();
 	const [countDown, setCountDown] = useState<number>(-1);
 	useEffect(() => {
 		if (idx === -1) return;
-		const intervalId = window.setInterval(() => {
+		intervalRef.current = window.setInterval(() => {
 			setCountDown((count) => {
 				// console.log(`count =>`, count);
 				return count - 1;
 			});
 		}, 1000);
-		return () => window.clearInterval(intervalId);
+		return cleanup;
 	}, [idx]);
 
 	useEffect(() => {
 		setCountDown(initialCount);
 	}, [initialCount]);
+
+	useEffect(() => {
+		if (countDown === 0) {
+			cleanup();
+		}
+	}, [countDown]);
+
+	const cleanup = () => {
+		if (intervalRef.current) {
+			window.clearInterval(intervalRef.current);
+			intervalRef.current = undefined;
+		}
+	};
 
 	return countDown;
 };
