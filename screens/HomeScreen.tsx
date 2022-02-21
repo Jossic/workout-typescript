eba271e0cdf78e04e448f3cb846b2f549f05a364
Jossic <jossic.lapierre@gmail.com>
2022-02-21 17:36:58 +0100

    Prepare to build

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,6 +1,6 @@
 import { NativeStackHeaderProps } from '@react-navigation/native-stack';
 import React from 'react';
-import { Pressable } from 'react-native';
+import { Pressable, useColorScheme } from 'react-native';
 import { FlatList, StyleSheet, Text, View } from 'react-native';
 import OrbitonText from '../components/styled/OrbitonText';
 import WorkoutItem from '../components/WorkoutItem';
@@ -10,10 +10,18 @@ type HomeScreenProps = NativeStackHeaderProps;
 
 const HomeScreen: React.FC<HomeScreenProps> = ({ navigation }) => {
 	const workouts = useWorkouts();
+	const colorScheme = useColorScheme();
+	const color = colorScheme === 'light' ? '#000' : '#fff';
 
 	return (
 		<View style={styles.container}>
-			<OrbitonText style={{ fontSize: 30 }}>Workouuuuts</OrbitonText>
+			<OrbitonText
+				style={{
+					fontSize: 30,
+					color: colorScheme === 'light' ? '#000' : '#fff',
+				}}>
+				Workouts
+			</OrbitonText>
 			<FlatList
 				data={workouts}
 				keyExtractor={(item) => item.slug}
